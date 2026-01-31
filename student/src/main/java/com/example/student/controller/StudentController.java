f1d77319087f8f2716083954afa4eadf0df089b3
akankshapatil098 <akankshapatil098@gmail.com>
2026-01-31 14:17:07 +0530

    Initial commit: Student Management System project

---
+++ student/src/main/java/com/example/student/controller/StudentController.java
@@ -0,0 +1,43 @@
+package com.example.student.controller;
+import com.example.student.model.Student;
+import com.example.student.service.StudentService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/students")
+public class StudentController {
+
+    private final StudentService service;
+
+    public StudentController(StudentService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public Student createStudent(@RequestBody Student student) {
+        return service.saveStudent(student);
+    }
+
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return service.getAllStudents();
+    }
+
+    @GetMapping("/{id}")
+    public Student getStudentById(@PathVariable Long id) {
+        return service.getStudentById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Student updateStudent(@PathVariable Long id, @RequestBody Student student) {
+        return service.updateStudent(id, student);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteStudent(@PathVariable Long id) {
+        service.deleteStudent(id);
+        return "Student deleted successfully";
+    }
+}
